637ba70174a6468c1eced798d66adc2f7426e2a9
mako95 <mako.2@hotmail.com>
2020-09-16 22:33:53 -0300

    Product Category Created

---
+++ MyShop/MyShop.Core/Models/ProductCategory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+    public class ProductCategory
+    {
+        public string Id { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Category { get; set; }
+
+        public ProductCategory()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+    }
+}
