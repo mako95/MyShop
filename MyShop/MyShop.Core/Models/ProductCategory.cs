de6dfa378607caa9e1505a034d64eabd51e9afe0
mako95 <mako.2@hotmail.com>
2020-09-17 23:46:24 -0300

    Generic Repository Implemented

--- MyShop/MyShop.Core/Models/ProductCategory.cs
+++ MyShop/MyShop.Core/Models/ProductCategory.cs
@@ -7,17 +7,10 @@ using System.Threading.Tasks;
 
 namespace MyShop.Core.Models
 {
-    public class ProductCategory
+    public class ProductCategory : BaseEntity
     {
-        public string Id { get; set; }
-
         [Required]
         [StringLength(20)]
         public string Category { get; set; }
-
-        public ProductCategory()
-        {
-            this.Id = Guid.NewGuid().ToString();
-        }
     }
 }
